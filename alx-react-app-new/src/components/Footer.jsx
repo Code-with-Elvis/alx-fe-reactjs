62812189648b9ec037af8502e17ba7e2b2fcb6ae
Code-with-Elvis <okothelvis137@gmail.com>
2025-09-05 23:16:08 +0300

    Apply Inline CSS Styling to React Components

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,18 @@
+const Footer = () => {
+  return (
+    <footer>
+      <p
+        style={{
+          textAlign: "center",
+          padding: "10px",
+          backgroundColor: "lightgray",
+          color: "black",
+        }}
+      >
+        © 2023 City Lovers
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
