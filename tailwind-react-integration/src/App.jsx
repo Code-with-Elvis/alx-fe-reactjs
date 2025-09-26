668cf9419e7eba41633441a4d7e1c0cb44073020
Elvis Okumu <okothelvis137@gmail.com>
2025-09-26 09:15:21 +0300

    style: Responsive Design Implementation Using Tailwind CSS

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -2,9 +2,9 @@ import UserProfile from "./components/UserProfile";
 
 const App = () => {
   return (
-    <>
+    <div className="p-4">
       <UserProfile />
-    </>
+    </div>
   );
 };
 export default App;
