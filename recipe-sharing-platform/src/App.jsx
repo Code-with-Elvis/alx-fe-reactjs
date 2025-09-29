a9dbeda25b596d50a8669fec0fd2d49d8e99d8f0
Elvis Okumu <okothelvis137@gmail.com>
2025-09-29 23:59:31 +0300

    Setting Up a React Application for a Recipe Sharing Platform with Tailwind CSS

---
+++ recipe-sharing-platform/src/App.jsx
@@ -0,0 +1,8 @@
+const App = () => {
+  return (
+    <div className="text-blue-500 py-4 px-8">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit.{" "}
+    </div>
+  );
+};
+export default App;
